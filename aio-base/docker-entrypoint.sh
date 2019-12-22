2ae9affc7f0a0a392ac2ac8fa18195a224aa2c3d
Tof <christophe.laborde@nbility.fr>
2019-12-22 01:30:30 +0100

    Update docker-entrypoint.sh

--- aio-base/docker-entrypoint.sh
+++ aio-base/docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+start_nginx() {
+	local conf=nginx.conf
+	[ -e /etc/nginx/certs/nginx.crt -a \
+	  -e /etc/nginx/certs/nginx.key -a \
+	  -e /etc/nginx/certs/dhparam.pem ] && conf=nginx-ssl.conf
+	envsubst '$NGINX_HOST $NGINX_PORT' < /etc/nginx/conf.d.templates/${conf} > /etc/nginx/conf.d/default.conf
+	service nginx start
+}
+
 start_postgres() {
 
 	if [ -s "$PGDATA/PG_VERSION" ]; then
